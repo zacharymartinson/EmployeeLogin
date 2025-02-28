92ac4463f733936a183f0b7202bd3da7f3574a89
PhlawedZM <zdmvideos@gmail.com>
2025-02-28 03:53:40 -0500

    Just some issues with scaling, but we have face detection.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
-    runtimeOnly(libs.face.detection)
+    implementation(libs.face.detection)
     implementation(libs.tensorflow.lite)
     implementation(libs.tensorflow.lite.gpu)
     implementation(libs.tensorflow.lite.gpu.api)
--- app/src/main/java/com/zachm/employeelogin/CameraActivity.kt
+++ app/src/main/java/com/zachm/employeelogin/CameraActivity.kt
@@ -1,22 +1,64 @@
 package com.zachm.employeelogin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
 import com.zachm.employeelogin.ui.screens.CameraScreen
 
 class CameraActivity : ComponentActivity() {
+    private val viewModel: CameraViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        requestPermissions()
+
         enableEdgeToEdge()
 
+        viewModel.detector.value = FaceDetection.getClient(viewModel.getDetectorOptions())
+
         val processCamera = ProcessCameraProvider.getInstance(this)
+        val thread = ContextCompat.getMainExecutor(this)
 
         setContent {
-            CameraScreen(processCamera)
+            CameraScreen(processCamera, this, thread)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        ProcessCameraProvider.getInstance(this).get().unbindAll()
+    }
+
+    private fun requestPermissions() {
+        permissionLauncher.launch(arrayOf(
+            android.Manifest.permission.CAMERA
+        ))
+    }
+
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        val allGranted = it.all { entry -> entry.value }
+        viewModel.permissionGranted.value = allGranted
+        Log.d("Permissions", "All permissions granted: $allGranted")
+
+        it.entries.forEach { entry ->
+            val permission = entry.key
+            val granted = entry.value
+
+            if(granted) {
+                Log.d("Permissions", "$permission granted.")
+            }
+            else {
+                Log.d("Permissions", "$permission denied.")
+            }
         }
     }
 }
