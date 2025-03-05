6956a95f427375f1ec6e8832aba2b930f0cff9ac
PhlawedZM <zdmvideos@gmail.com>
2025-03-04 19:59:40 -0500

    Face Detection Working with Proper support for any CameraX Resolution.

--- app/src/main/java/com/zachm/employeelogin/CameraViewModel.kt
+++ app/src/main/java/com/zachm/employeelogin/CameraViewModel.kt
@@ -19,7 +19,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetector
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -27,20 +26,23 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeout
 import java.util.concurrent.Executor
+import kotlin.math.abs
 
 class CameraViewModel : ViewModel() {
 
     val permissionGranted: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>(false) }
     val detector: MutableLiveData<FaceDetector> by lazy { MutableLiveData<FaceDetector>() }
 
-
     private val _bboxes = MutableStateFlow<List<Rect>?>(null)
     val bboxes: StateFlow<List<Rect>?> get() = _bboxes
 
-    private val _imageSize = MutableStateFlow<IntSize?>(null)
-    val imageSize: StateFlow<IntSize?> get() = _imageSize
-
-    fun updateCameraFeed(processCamera: ListenableFuture<ProcessCameraProvider>, surfaceProvider: Preview.SurfaceProvider, lifeCycleOwner: LifecycleOwner, thread: Executor) {
+    fun updateCameraFeed(
+        processCamera: ListenableFuture<ProcessCameraProvider>,
+        surfaceProvider: Preview.SurfaceProvider,
+        lifeCycleOwner: LifecycleOwner,
+        thread: Executor,
+        screenSize: IntSize
+    ) {
 
         if(permissionGranted.value == false) return
 
@@ -60,8 +62,7 @@ class CameraViewModel : ViewModel() {
                 .build()
 
             imageAnalysis.setAnalyzer(thread) {
-                _imageSize.value = IntSize(it.width, it.height)
-                detectFaces(it)
+                detectFaces(it, screenSize)
             }
 
             try {
@@ -79,19 +80,22 @@ class CameraViewModel : ViewModel() {
 
 
     @OptIn(ExperimentalGetImage::class)
-    fun detectFaces(proxy: ImageProxy) {
+    fun detectFaces(proxy: ImageProxy, screenSize: IntSize) {
         proxy.image?.let { ///Null check
             viewModelScope.launch {
-                withTimeout(5000) {
+                withTimeout(10000) {
                     try {
                         val inputImage = InputImage.fromMediaImage(it, proxy.imageInfo.rotationDegrees)
                         detector.value!!.process(inputImage)
                             .addOnSuccessListener {
                                 val boxes = mutableListOf<Rect>()
+
                                 it.forEach { face ->
-                                    boxes.add(face.boundingBox)
+                                    val box = getScaledRect(screenSize, IntSize(proxy.width, proxy.height), face.boundingBox, proxy.imageInfo.rotationDegrees)
+                                    boxes.add(box)
                                 }
                                 _bboxes.value = boxes
+
                                 proxy.close()
                             }
                             .addOnFailureListener {
@@ -103,6 +107,48 @@ class CameraViewModel : ViewModel() {
                         Log.d("CameraViewModel", "Exception: $e")
                     }
                 }
+            }
+        }
+    }
+
+    /**
+     * Scales the bounding box to the screen size allowing for different camera sizes.
+     */
+    private fun getScaledRect(screenSize: IntSize, cameraSize: IntSize, rect: Rect, rotation: Int): Rect {
+        val isPortrait = rotation == 90 || rotation == 270
+
+        val widthRatio = if(isPortrait) screenSize.width.toFloat() / cameraSize.height.toFloat() else screenSize.width.toFloat() / cameraSize.width.toFloat()
+        val heightRatio = if(isPortrait) screenSize.height.toFloat() / cameraSize.width.toFloat() else screenSize.height.toFloat() / cameraSize.height.toFloat()
+
+        val cameraAspectRatio = cameraSize.width.toFloat() / cameraSize.height.toFloat()
+        val screenAspectRatio = screenSize.width.toFloat() / screenSize.height.toFloat()
+        val aspectRatioDiff = abs(cameraAspectRatio - screenAspectRatio)
+
+        val box = Rect(
+            (rect.left * widthRatio).toInt(),
+            (rect.top * heightRatio).toInt(),
+            (rect.right * widthRatio).toInt(),
+            (rect.bottom * heightRatio).toInt()
+        )
+
+        val padding = 0.8f
+
+        when {
+            isPortrait -> {
+                return Rect(
+                    box.left - (box.width() * padding).toInt(),
+                    box.top - ((aspectRatioDiff * box.height())/4).toInt(),
+                    box.right + (box.width() * padding).toInt(),
+                    box.bottom + ((aspectRatioDiff * box.height())/4).toInt()
+                )
+            }
+            else -> {
+                return Rect(
+                    box.left - ((aspectRatioDiff * box.width())/4).toInt(),
+                    box.top - (box.height() * padding).toInt(),
+                    box.right + ((aspectRatioDiff * box.width())/4).toInt(),
+                    box.bottom + (box.height() * padding).toInt()
+                )
             }
         }
     }
